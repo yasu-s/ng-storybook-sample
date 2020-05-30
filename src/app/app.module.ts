7c775664d958cf4ad104365e8bd9744f763d55b9
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2020-05-30 12:21:12 +0900

    フォルダをフラット化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SHARED_COMPONENTS } from './components';
+import { CustomButtonComponent } from './custom-button.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, SHARED_COMPONENTS],
+  declarations: [AppComponent, CustomButtonComponent],
   imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/index.ts
+++
@@ -1,5 +0,0 @@
-/* tslint:disable:max-line-length */
-import { CustomButtonComponent } from './custom-button/custom-button.component';
-
-/** Shared Components */
-export const SHARED_COMPONENTS = [CustomButtonComponent];
