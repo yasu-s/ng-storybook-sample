0029b433871525b085bcf8d2715f3160ed0885ff
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2020-05-30 15:15:38 +0900

    コンポーネントを整理

--- src/app/custom-button.component.ts
+++ src/app/custom-button.component.ts
@@ -1,17 +1,9 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'custom-button',
-  templateUrl: './custom-button.component.html',
+  template: `<button>{{ label }}</button>`,
 })
 export class CustomButtonComponent {
-  /** ラベル */
   @Input() label = 'button';
-
-  /** クリックイベント */
-  @Output() btnClick = new EventEmitter();
-
-  onClick(): void {
-    this.btnClick.emit();
-  }
 }
